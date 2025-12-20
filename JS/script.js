8c1f68052686479851f39f0dec4bfb88cd9d14a0
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-20 12:01:20 +0545

    variable in js

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,39 @@ console.log("Hello, World!");
 // let a = prompt("Enter your name ");
 // console.log("Welcome " + a);
 
-let x = confirm("Are you sure you want to delete this page?");
+// let x = confirm("Are you sure you want to delete this page?");
 
-if(x){
-    console.log("Page Deleted");
-}
-else{
-    console.log("Page Not Deleted");
-}
+// if(x){
+//     console.log("Page Deleted");
+// }
+// else{
+//     console.log("Page Not Deleted");
+// }
+
+var a = 1;
+var b = 45;
+a=a+2;
+console.log("The sum of a and b is: " + (a + b));
+
+console.log(a);
+
+ {
+    var a = a+4;
+    console.log(a);
+
+ }
+ console.log(a);
+
+ let c =3;
+ console.log(c);
+
+ {
+    let c = 12;
+    console.log(c);
+ }
+console.log(c);
+
+const d = 10 ;
+
+// d = d+10; // cannot change
+console.log(d);
