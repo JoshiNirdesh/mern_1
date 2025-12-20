99508d0967be140ad33603b92dd479fcb9f09a49
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-20 14:37:17 +0545

    array

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
 
 // alert("This is a Alert Message");
 // let a = prompt("Enter your name ");
@@ -15,20 +15,101 @@ console.log("Hello, World!");
 
 // 
 
-let a = 10;
-let name = "Ram";
+// let a = 10;
+// let name = "Ram";
 
-let q =undefined;
-let z = null;
+// let q =undefined;
+// let z = null;
 
-console.log(a, name , q , z);
+// console.log(a, name , q , z);
 
-console.log(typeof a, typeof name , typeof q , typeof z);
+// console.log(typeof a, typeof name , typeof q , typeof z);
 
-let user = {
-   name : "Ram",
-   age : 12
-}
+// let user = {
+//    name : "Ram",
+//    age : 12
+// }
 
-console.log ("Name : "+ user.name +"Age : "+ user.age);
+// console.log ("Name : "+ user.name +"Age : "+ user.age);
+
+// let a = prompt("Enter a first number");
+// let b = prompt("Enter a second number");
+
+// let c = prompt("Enter a operator");
+
+// let d = Math.random();
+// console.log(d)
+
+// let obj = {
+//     "+":"-",
+//     "-":"+",
+//     "*":"/",
+//     "/":"*"
+// }
+// if (d>0.1){
+//     alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+// }
+// else{ 
+//     c= obj[c];
+//     alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+// }
+
+let numbers = [1,2,3,4,5,6];
+
+console.log(numbers.toString());
+console.log(numbers.join("-"));
+
+numbers.pop();
+console.log(numbers);
+
+numbers.push(12);
+console.log(numbers);
+
+numbers.shift();
+console.log(numbers);
+
+numbers.unshift(100);
+console.log(numbers)
+
+
+console.log(numbers.length);
+
+let a = [1,2,3]
+let b = [4,5,6]
+let c = [7,8,9];
+
+let d = a.concat(b,c);
+
+console.log(d);
+
+let num = [1,4,5,67,8,4];
+
+num.splice(1,2);
+console.log(num);
+
+num.splice(1,3,6,7);
+console.log(num)
+
+let numb = [1,2,3,4,5,6,7,8];
+
+numb.forEach((value,index)=>{
+   console.log(value);
+})
+
+numb.map((e)=>{
+   console.log(e+2);
+})
+
+let even =  numb.filter(numb => numb%2==0);
+console.log(even);
+
+let users =  [
+   {name: "Ram", age : 6},
+   {name : "Shyam", age : 17},
+   {name: "hari", age:12}
+]
+
+let newUser = users.filter(u=>u.age>10 );
+console.log(newUser);
+
 
