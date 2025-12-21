47118979c92432372725a5a31c8443e2914fcb09
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-21 10:58:35 +0545

    dom

---
+++ JS/dom.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    btn.textContent="Clicked"
+});
+
+
+let input = document.querySelector(".input");
+let output =document.querySelector(".output");
+
+input.addEventListener("input",(e)=>{
+    output.textContent = e.target.value
+})
