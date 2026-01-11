b22de1db7f0c58ddaf78febb4f06ab8a18f3f9e9
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-01-11 17:37:02 +0545

    mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const connectDb = require("./config/dbconfig");
 const app = express();
 
 dotenv.config();
@@ -7,6 +8,8 @@ dotenv.config();
 app.get("/", (req, res) => {
     res.send("This is home page");
 })
+connectDb();
+
 app.use("/api/v1/test", require("./routes/testRoutes"));
 const port = process.env.port
 
